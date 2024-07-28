901cfc6c8d936e88a04328af453a531da2a8cbd0
Mokhirboy Muzaffarov <mokhirboy.muzaffarov@trackstreet.com>
2024-07-28 14:18:47 +0500

    edited host in swagger

--- docs/docs.go
+++ docs/docs.go
@@ -171,7 +171,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:3000",
+	Host:             "skinai.up.railway.app",
 	BasePath:         "/api/v1",
 	Schemes:          []string{},
 	Title:            "Skin Ai Swagger",
--- main.go
+++ main.go
@@ -18,7 +18,7 @@ import (
 // @license.name Apache 2.0
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
 
-// @host localhost:3000
+// @host skinai.up.railway.app
 // @BasePath /api/v1
 
 
