77fd39681f236a6f0eb53565c9f5328ce414a751
Mokhirboy Muzaffarov <mokhirboy.muzaffarov@trackstreet.com>
2024-08-24 23:33:39 +0500

    updated swag

--- internal/delivery/rest/user_info_controller.go
+++ internal/delivery/rest/user_info_controller.go
@@ -50,7 +50,7 @@ func (c controller) FillUserInfo(ctx *gin.Context) {
 // @Tags users
 // @Accept  json
 // @Produce  json
-// @Params user_email body dto.UserEmail true "User email"
+// @Params UserEmail body dto.UserEmail true "User email"
 // @Success 200 {object} map[string]interface{} "Success response"
 // @Failure 400 {object} map[string]string "Error response"
 // @Failure 406 {object} map[string]string "Invalid request payload"
