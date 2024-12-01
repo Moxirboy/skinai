5d5eb40c0ae39090dbb3b3015958ba463ef24eec
Mokhirboy Muzaffarov <mokhirboy.muzaffarov@trackstreet.com>
2024-12-01 11:55:11 +0500

    domain change

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ import (
 // @license.name Apache 2.0
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
 
-// @host web.binaryhood.uz
+// @host skinai.up.railway.app/
 // @BasePath /api/v1
 
 
