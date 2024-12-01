88610f9e570367caaa65e0004020102830c17575
Mokhirboy Muzaffarov <mokhirboy.muzaffarov@trackstreet.com>
2024-12-01 11:57:08 +0500

    domain change

--- docs/docs.go
+++ docs/docs.go
@@ -809,7 +809,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "web.binaryhood.uz",
+	Host:             "skinai.up.railway.app/",
 	BasePath:         "/api/v1",
 	Schemes:          []string{},
 	Title:            "Skin Ai Swagger",
